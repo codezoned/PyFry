2a3df1d7b59b5b355c5b1dd3aee03396f68cd8ab
Sagar Vakkala <sagarwakawaka@gmail.com>
2019-03-16 22:03:17 +0530

    Added PyFry and Utils

---
+++ PyFry.py
@@ -0,0 +1,8 @@
+import cv2
+from PIL import Image
+
+'''
+TODO: -> Compressing (Crushing) and back (to increase noise)
+      -> Applying Red and Orange hue filters for classic deep fry look
+      -> Detecting eye coordinates and applying the deepfry eye flare in the center
+'''
