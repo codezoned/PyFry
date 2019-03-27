f2a457e572eec763c9a7be430a92fc3508976b11
Sagar Vakkala <sagarwakawaka@gmail.com>
2019-03-27 09:49:17 +0530

    Fixed crushAndBack

--- PyFry.py
+++ PyFry.py
@@ -1,6 +1,7 @@
 import cv2
 from PIL import Image
 import os
+import imutils
 '''
 TODO: -> Compressing (Crushing) and back (to increase noise)
       -> Applying Red and Orange hue filters for classic deep fry look
@@ -9,16 +10,20 @@ TODO: -> Compressing (Crushing) and back (to increase noise)
 def crushAndBack(img):
     img = img.convert('RGB')
     w,h = img.width, img.height
-    img = img.resize(((int(w ** 0.7), int(h ** 0.7)), resample = Image.LANCZOS))
-    img = img.resize(((int(w ** 0.6), int(h ** 0.6)), resample = Image.BILINEAR))
-    img = img.resize(((int(w ** 0.5), int(h ** 0.5)), resample = Image.BICUBIC))
+    img = img.resize((int(w ** .95), int(h ** .95)), resample=Image.LANCZOS)
+    img = img.resize((int(w ** .85), int(h ** .85)), resample = Image.BILINEAR)
+    img = img.resize((int(w ** .70), int(h ** .70)), resample = Image.BICUBIC)
     img = img.resize((w,h), resample = Image.BICUBIC)
     return img
 
+
+
 def main():
-    img = Image.open('test.jpeg')
-    img = crushAndBack(img)    
-    img.save('sample.jpg','jpeg')
+    img = Image.open('test.jpg')
+    img = img.convert('RGB')
+    img = crushAndBack(img)
+    img.show()
+    #img.save('sample.jpg','jpeg')
 
 if __name__ == '__main__':
     main()
