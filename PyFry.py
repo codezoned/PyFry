f28ce525321fe6486ebbcb7c24226a2d94a099a1
Sagar Vakkala <sagarwakawaka@gmail.com>
2019-04-07 09:17:32 +0530

    Fixed bugs
    Out of index errors were fixed for shape[]

--- PyFry.py
+++ PyFry.py
@@ -18,26 +18,25 @@ def crushAndBack(img):
     return img
 
 def main():
-    img = Image.open('test.jpg')
-    img = img.convert('RGB')
-    imgCV = cv2.imread('test.jpg')
-
     # Initialising dlib for frontal facial features
     detect = dlib.get_frontal_face_detector()
-    predict = dlib.shape_predictor("E:\Github projects\PyFry\assets\shape_predictor_68_face_landmarks.dat")
+    predict = dlib.shape_predictor("assets\shape_predictor_68_face_landmarks.dat")
 
     (lS, lE) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
     (rS, rE) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+    
+    imgCV = cv2.imread('test.jpg')
 
     gray = cv2.cvtColor(imgCV, cv2.COLOR_BGR2GRAY)
     subjects = detect(gray, 0)
     for subject in subjects:
         shape = predict(gray, subject)
         shape = face_utils.shape_to_np(shape)
-        leftEye = shape[lS, lE]
-        rightEye = shape[rS, rE]
+        leftEye = shape[lS:lE]
+        rightEye = shape[rS:rE]
         print(leftEye)
-    img.show()
+    cv2.imshow("Frame", imgCV)
+    cv2.waitKey(0)
     #img.save('sample.jpg','jpeg')
 
 if __name__ == '__main__':
